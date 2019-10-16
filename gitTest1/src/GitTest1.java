55a8d46cfee9de2f85fe133953ea0fac449e0051
lonecool <1415216905@qq.com>
2019-10-16 12:12:58 +0800

    分支提交

--- gitTest1/src/GitTest1.java
+++ gitTest1/src/GitTest1.java
@@ -1,6 +1,7 @@
 public class GitTest1 {
     public static void main(String[] args) {
-        System.out.println("你好，再见");
-        System.out.println("安河桥");
-    }
+    System.out.println("你好，再见");
+    System.out.println("安河桥");
+    System.out.println("东");
+}
 }
